cffc9a655d463b994b1f185e888be8b4ee56bb41
zcg <zhengchunguang66@gmail.com>
2026-05-21 19:05:27 +0800

    [ADD]

---
+++ assets/scripts/hall/room/JoinRoomPopup.ts
@@ -0,0 +1,139 @@
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class JoinRoomPopup extends cc.Component {
+
+    private mask: cc.Node = null!;
+    private panel: cc.Node = null!;
+    private closeBtn: cc.Node = null!;
+    private enterBtn: cc.Node = null!;
+
+    private inputItems: cc.Node[] = [];
+    private keyRoot: cc.Node = null!;
+
+    private roomId: string = "";
+    private readonly maxLen: number = 6;
+
+    onLoad() {
+        this.node.active = false;
+
+        this.mask = this.node.getChildByName("Mask");
+        this.panel = this.node.getChildByName("Panel");
+
+        this.closeBtn = this.panel.getChildByName("CloseBtn");
+        this.enterBtn = this.panel.getChildByName("EnterBtn");
+
+        const inputRoot = this.panel.getChildByName("InputRoot");
+        this.inputItems = inputRoot.children;
+
+        this.keyRoot = this.panel.getChildByName("Keyboard");
+
+        this.bindEvents();
+    }
+
+    private bindEvents() {
+        this.mask.on(cc.Node.EventType.TOUCH_END, this.hide, this);
+        this.closeBtn.on(cc.Node.EventType.TOUCH_END, this.hide, this);
+        this.enterBtn.on(cc.Node.EventType.TOUCH_END, this.onEnterRoom, this);
+
+        for (let i = 0; i <= 9; i++) {
+            const key = this.keyRoot.getChildByName("Key" + i);
+            if (key) {
+                key.on(cc.Node.EventType.TOUCH_END, () => {
+                    this.inputNumber(i);
+                }, this);
+            }
+        }
+
+        const delBtn = this.keyRoot.getChildByName("KeyDel");
+        const clearBtn = this.keyRoot.getChildByName("KeyClear");
+
+        delBtn && delBtn.on(cc.Node.EventType.TOUCH_END, this.deleteNumber, this);
+        clearBtn && clearBtn.on(cc.Node.EventType.TOUCH_END, this.clearNumber, this);
+    }
+
+    show() {
+        this.node.active = true;
+        this.roomId = "";
+        this.refreshInput();
+
+        this.mask.opacity = 0;
+        this.panel.scale = 0.85;
+        this.panel.opacity = 0;
+
+        cc.tween(this.mask)
+            .to(0.2, { opacity: 180 })
+            .start();
+
+        cc.tween(this.panel)
+            .to(0.25, {
+                scale: 1,
+                opacity: 255
+            }, { easing: "backOut" })
+            .start();
+    }
+
+    hide() {
+        cc.tween(this.mask)
+            .to(0.15, { opacity: 0 })
+            .start();
+
+        cc.tween(this.panel)
+            .to(0.15, {
+                scale: 0.85,
+                opacity: 0
+            })
+            .call(() => {
+                this.node.active = false;
+            })
+            .start();
+    }
+
+    private inputNumber(num: number) {
+        if (this.roomId.length >= this.maxLen) {
+            return;
+        }
+
+        this.roomId += num.toString();
+        this.refreshInput();
+    }
+
+    private deleteNumber() {
+        if (this.roomId.length <= 0) {
+            return;
+        }
+
+        this.roomId = this.roomId.substring(0, this.roomId.length - 1);
+        this.refreshInput();
+    }
+
+    private clearNumber() {
+        this.roomId = "";
+        this.refreshInput();
+    }
+
+    private refreshInput() {
+        for (let i = 0; i < this.inputItems.length; i++) {
+            const item = this.inputItems[i];
+            const label = item.getComponentInChildren(cc.Label);
+
+            if (label) {
+                label.string = this.roomId[i] || "";
+            }
+        }
+    }
+
+    private onEnterRoom() {
+        if (this.roomId.length < this.maxLen) {
+            cc.log("请输入完整房号");
+            return;
+        }
+
+        cc.log("进房，房号：", this.roomId);
+
+        // TODO 调用进房协议
+        // GameNet.joinRoom(this.roomId);
+
+        this.hide();
+    }
+}
