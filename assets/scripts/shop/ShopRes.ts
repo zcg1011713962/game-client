50b410aa3a68a6df91bbc79eb30813dbd5f54daa
zcg1011713962 <zcg1011713962@163.com>
2026-05-25 14:08:09 +0800

    [ADD]

---
+++ assets/scripts/shop/ShopRes.ts
@@ -0,0 +1,58 @@
+export default class ShopRes {
+    private static _instance: ShopRes = null;
+
+    private shopBundle: cc.AssetManager.Bundle = null;
+    public shopPrefab: cc.Prefab = null;
+
+    public static get instance(): ShopRes {
+        if (!this._instance) {
+            this._instance = new ShopRes();
+        }
+        return this._instance;
+    }
+
+    private constructor() {}
+
+
+    private loadShopBundle(): Promise<cc.AssetManager.Bundle> {
+        return new Promise((resolve, reject) => {
+            if (this.shopBundle) {
+                resolve(this.shopBundle);
+                return;
+            }
+
+            cc.assetManager.loadBundle("bundle_shop", (err, bundle) => {
+                if (err) {
+                    cc.error("bundle_shop 加载失败:", err);
+                    reject(err);
+                    return;
+                }
+
+                this.shopBundle = bundle;
+                resolve(bundle);
+            });
+        });
+    }
+
+    public async loadShopPrefab(): Promise<cc.Prefab> {
+        if (this.shopPrefab) return this.shopPrefab;
+
+        const bundle = await this.loadShopBundle();
+
+        return new Promise((resolve, reject) => {
+            bundle.load("prefabs/Shop", cc.Prefab, (err, prefab: cc.Prefab) => {
+                if (err) {
+                    cc.error("商城预制体加载失败:", err);
+                    reject(err);
+                    return;
+                }
+
+                this.shopPrefab = prefab;
+
+                cc.log("商城预制体加载完成");
+                resolve(prefab);
+            });
+        });
+    }
+
+}
